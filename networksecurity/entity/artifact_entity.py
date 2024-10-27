3029ba173d30ddc0b7af7ca5e180aae0dbb7ccdd
abhinav kumar <abhinav.mg.k@gmail.com>
2024-10-27 23:38:28 +0530

    config and artifact structure updated

---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    pass
+
+@dataclass
+class DataTansformationArtifact:
+    pass
+
+@dataclass
+class DataValidationArtifact:
+    pass
+
+@dataclass
+class ModelTranierArtifact:
+    pass
+
+@dataclass
+class ModelEvaluationArtifact:
+    pass
+
+@dataclass
+class ModelPusherArtifact:
+    pass
+
+@dataclass
+class ClassificationMatrixArtifact:
+    pass
