a26d37be528d54661c89edc6da6101ded10fc50e
abhinav kumar <abhinav.mg.k@gmail.com>
2024-10-28 17:39:43 +0530

    data ingestion complete

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -9,39 +9,66 @@ import numpy as np
 import pymongo
 from typing import List
 from dotenv import load_dotenv
+from sklearn.model_selection import train_test_split
 
 load_dotenv()
 MONGO_DB_URL = os.getenv('MONGO_DB_URL')
 
 class DataIngestion:
-    def __init__(self):
+    def __init__(self,data_ingestion_config: DataIngestionConfig):
         try:
-            pass
+            self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
 
     def export_collection_as_dataframe(self):
         try:
-            pass
+            self.database_name = self.data_ingestion_config.database_name
+            self.collection_name = self.data_ingestion_config.collection_name
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.collection = self.mongo_client[self.database_name][self.collection_name]
+            df = pd.DataFrame(list(self.collection.find()))
+            if "_id" in df.columns.tolist():
+                df = df.drop(columns=["_id"],axis=1)
+            df.replace({"na":np.nan},inplace=True)
+            return df
+        
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def export_data_into_feature_store(self):
+    def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
-            pass
+            self.feature_store_file_path = self.data_ingestion_config.feature_store_file_path
+            dir_path = os.path.dirname(self.feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            dataframe.to_csv(self.data_ingestion_config.feature_store_file_path, index=False,header=True)
+            return dataframe
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def split_data_as_train_test(self):
+    def split_data_as_train_test(self,dataframe: pd.DataFrame):
         try:
-            pass
+            train_set, test_set = train_test_split(dataframe,test_size=self.data_ingestion_config.train_test_split_ratio)
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            logging.info(f"Exporting training and test file")
+            os.makedirs(dir_path,exist_ok=True)
+            train_set.to_csv(self.data_ingestion_config.training_file_path,index=False,header=True)
+            test_set.to_csv(self.data_ingestion_config.test_file_path,index=False,header=True)
+            logging.info(f"train and test file export completed successfully")
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
     def inititate_data_ingestion(self):
         try:
-            pass
+            dataframe = self.export_collection_as_dataframe()
+            dataframe = self.export_data_into_feature_store(dataframe=dataframe)
+            self.split_data_as_train_test(dataframe=dataframe)
+            data_ingestion_artifact = DataIngestionArtifact(
+                trained_file_path = self.data_ingestion_config.training_file_path,
+                test_file_path = self.data_ingestion_config.test_file_path
+            )
+            return data_ingestion_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+"""Defining common constant variables for training pipeline"""
+PIPELINE_NAME: str = 'NetworkSecurity'
+ARTIFACT_DIR: str = 'Artifacts'
+FILE_NAME: str = 'NetworkData.csv'
+TARGET_COLUMN: str = 'Result'
+
+TRAIN_FILE_NAME: str = 'train.csv'
+TEST_FILE_NAME: str = 'test.csv'
+PREPROCESSING_OBJECT_FILE_NAME: str = 'preprocessing.pkl'
+MODEL_FILE_NAME: str = 'model.pkl'
+SCHEMA_FILE_PATH: str = os.path.join("data_schema","schema.yaml")
+SCHEMA_DROP_COLS: str = "drop_columns"
+SAVED_MODEL_DIR: str = os.path.join("saved_models")
+
+"""Data Ingestion related constants start with DATA_INGESTION var name"""
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkDataBase"
+DATA_INGESTION_DATABASE_NAME: str = "NetworkDataCollection"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+"""Data Transformation related constants start with DATA_TRANSFORMATION var name"""
+
+"""Data validation related constants start with DATA_VALIDATION var name"""
+
+"""Model Trainer related constants start with MODEL_TRAINER var name"""
